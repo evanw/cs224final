4d431651779c637c320eb55d2f6de3683a2659aa
Justin Ardini <jardini@cs.brown.edu>
2011-04-23 17:22:56 -0400

    started catmull clark, setup data structures for it

---
+++ lib/catmullclark.h
@@ -0,0 +1,47 @@
+#ifndef CATMULLCLARK_H
+#define CATMULLCLARK_H
+
+#include <QVector>
+#include "mesh.h"
+
+// a face in a CatmullMesh
+struct CatmullFace {
+    CatmullFace(int numSides);
+
+    int n;
+    Vertex facePoint;
+    // these are filled to the correct size in the constructor, so they can be treated like arrays after construction
+    QVector<const CatmullFace *> neighbors;
+    QVector<int> points;
+    QVector<int> edgePoints;
+};
+
+// a vertex in a CatmullMesh
+struct CatmullVertex {
+    Vector3 pos;
+    Vector3 normal;
+
+    QVector<Vector3> facePoints; // face points for faces including this vertex
+    QVector<Vector3> edgePoints; // edge points for faces including this vertex
+};
+
+
+class CatmullMesh {
+public:
+    // create the mesh Catmull mesh from a regular mesh
+    CatmullMesh(const Mesh &m);
+
+    bool convertToMesh(Mesh &m);
+    static bool subdivide(const Mesh &in, Mesh &out);
+
+private:
+    QList<CatmullVertex> vertices;
+    QList<Vertex> edgePoints;
+    QList<CatmullFace> faces;
+
+    void fillNeighbors(CatmullFace &face);
+    bool moveVertices();
+};
+
+
+#endif // CATMULLCLARK_H
