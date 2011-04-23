e289e271d06ffac5827ce3c3d8083b350e17d2fa
Lance Jabr <lancejabr@gmail.com>
2011-04-23 16:52:18 -0400

    cleaned meshconstruction.cpp, at the moment it only sweep at the leaf nodes. more to come!

--- b_mesh/meshconstruction.cpp
+++ b_mesh/meshconstruction.cpp
@@ -1,6 +1,11 @@
 #include "meshconstruction.h"
 
 void MeshConstruction::BMeshInit(Mesh *m){
+    //empty the mesh
+    m->vertices.clear();
+    m->quads.clear();
+    m->triangles.clear();
+
     //find the root node
     Ball *root = NULL;
     foreach(Ball b, m->balls){
@@ -20,8 +25,11 @@ void MeshConstruction::sweep(Mesh *m, const Ball *b){
     Vector3 Z = Vector3(0,0,1);
 
     if(b->childrenIndices.size() > 0){
-        foreach(int i, b->childrenIndices)
+        foreach(int i, b->childrenIndices){
+            //sweep the child
             sweep(m, &m->balls.at(i));
+            //the last quad on the mesh
+        }
     } else if(b->childrenIndices.size() == 0){
         //this is an end node. find the local vectors
         Ball parent = m->balls.at(b->parentIndex);
@@ -36,27 +44,32 @@ void MeshConstruction::sweep(Mesh *m, const Ball *b){
             z = x.cross(y);
         }
 
+        float r = b->maxRadius();
+        x*=r;
+        y*=r;
+        z*=r;
+
         //make the quad cap
         Vertex v0, v1, v2, v3, v4, v5, v6, v7;
         v4.pos = b->center;
         v5.pos = b->center;
         v6.pos = b->center;
         v7.pos = b->center;
-        float r = b->maxRadius();
+
         //these verts are the center of the sphere
-        v4.pos += y*r;
-        v4.pos += z*r;
-        v5.pos -= y*r;
-        v5.pos += z*r;
-        v6.pos -= y*r;
-        v6.pos -= z*r;
-        v7.pos += y*r;
-        v7.pos -= z*r;
+        v4.pos += y;
+        v4.pos += z;
+        v5.pos -= y;
+        v5.pos += z;
+        v6.pos -= y;
+        v6.pos -= z;
+        v7.pos += y;
+        v7.pos -= z;
         //these are the edge of the sphere
-        v0.pos = v4.pos + x*r;
-        v1.pos = v5.pos + x*r;
-        v2.pos = v6.pos + x*r;
-        v3.pos = v7.pos + x*r;
+        v0.pos = v4.pos + x;
+        v1.pos = v5.pos + x;
+        v2.pos = v6.pos + x;
+        v3.pos = v7.pos + x;
 
         //add the cap vertices to the mesh, keep track of the start index
         int startIndex = m->vertices.size();
@@ -85,36 +98,33 @@ void MeshConstruction::sweep(Mesh *m, const Ball *b){
         cap.a = i0;
         cap.b = i1;
         cap.c = i2;
-        cap.d = i4;
+        cap.d = i3;
         m->quads.push_back(cap);
 
         //now add the sweep to the center of the sphere
         Quad q0, q1, q2, q3;
-        q0.a = i0;
-        q0.b = i1;
-        q0.c = i5;
-        q0.d = i4;
-        q1.a = i1;
-        q1.b = i2;
-        q1.c = i6;
-        q1.d = i5;
-        q2.a = i2;
-        q2.b = i3;
-        q2.c = i7;
-        q2.d = i6;
-        q3.a = i3;
-        q3.b = i0;
-        q3.c = i4;
-        q3.d = i7;
+        q0.a = i4;
+        q0.b = i5;
+        q0.c = i1;
+        q0.d = i0;
+        q1.a = i5;
+        q1.b = i6;
+        q1.c = i2;
+        q1.d = i1;
+        q2.a = i6;
+        q2.b = i7;
+        q2.c = i3;
+        q2.d = i2;
+        q3.a = i7;
+        q3.b = i4;
+        q3.c = i0;
+        q3.d = i3;
 
         m->quads.push_back(q0);
         m->quads.push_back(q1);
         m->quads.push_back(q2);
         m->quads.push_back(q3);
-
     }
-
-
 }
 
 void MeshConstruction::stitch(Mesh *m){
