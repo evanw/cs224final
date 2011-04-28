5206a71649f5c478ad857e39c6c83612bca9ee4e
Evan Wallace <evan_wallace@brown.edu>
2011-04-27 21:51:36 -0400

    added subdivision of bones

--- b_mesh/meshconstruction.cpp
+++ b_mesh/meshconstruction.cpp
@@ -124,6 +124,60 @@ static void addSweep(Mesh &mesh, int i0, int i1, int i2, int i3, const Vector3 &
     mesh.quads += Quad(i + 3, i, i0, i3);
 }
 
+static void addSegmentedSweep(Mesh &mesh,
+                              ResultQuad &startQuad,
+                              const Vector3 &end0, const Vector3 &end1, const Vector3 &end2, const Vector3 &end3,
+                              float startRadius, float endRadius)
+{
+    Vector3 start = (mesh.vertices[startQuad.i0].pos + mesh.vertices[startQuad.i1].pos + mesh.vertices[startQuad.i2].pos + mesh.vertices[startQuad.i3].pos) / 4;
+    Vector3 end = (end0 + end1 + end2 + end3) / 4;
+    float startToEnd = (end - start).length();
+    const int divisions = max(0, (startToEnd - startRadius - endRadius) / (startRadius + endRadius));
+    int i0 = startQuad.i0;
+    int i1 = startQuad.i1;
+    int i2 = startQuad.i2;
+    int i3 = startQuad.i3;
+
+    for (int i = 0; i <= divisions; i++)
+    {
+        if (i == divisions)
+        {
+            // special-case the end, which is at a different angle
+            mesh.vertices += end0;
+            mesh.vertices += end1;
+            mesh.vertices += end2;
+            mesh.vertices += end3;
+        }
+        else
+        {
+            // calculate how far along the bone we are, starting from after startRadius and ending before endRadius
+            // this will go negative if the spheres are intersecting, but in that case divisions == 0 so it doesn't matter
+            float percent = (divisions > 1) ? (float)i / (float)(divisions - 1) : 0;
+            float scale = (startRadius + (startToEnd - startRadius - endRadius) * percent) / startToEnd;
+            Vector3 offset = start + (end - start) * scale;
+
+            // interpolate the position along the bone, growing or shrinking based on startRadius, endRadius, and how far along the bone we are
+            scale = (startRadius + (endRadius - startRadius) * scale) / startRadius;
+            mesh.vertices += offset + (startQuad.v[0] - start) * scale;
+            mesh.vertices += offset + (startQuad.v[1] - start) * scale;
+            mesh.vertices += offset + (startQuad.v[2] - start) * scale;
+            mesh.vertices += offset + (startQuad.v[3] - start) * scale;
+        }
+
+        // generate the quads
+        int i = mesh.vertices.count() - 4;
+        mesh.quads += Quad(i, i + 1, i1, i0);
+        mesh.quads += Quad(i + 1, i + 2, i2, i1);
+        mesh.quads += Quad(i + 2, i + 3, i3, i2);
+        mesh.quads += Quad(i + 3, i, i0, i3);
+
+        i0 = i;
+        i1 = i + 1;
+        i2 = i + 2;
+        i3 = i + 3;
+    }
+}
+
 static void makeElbow(Mesh &mesh, const Ball &ball, ResultQuad &result)
 {
     Ball &child = mesh.balls[ball.childrenIndices[0]];
@@ -133,6 +187,7 @@ static void makeElbow(Mesh &mesh, const Ball &ball, ResultQuad &result)
     if (ball.parentIndex == -1 || last.invalid)
     {
         // TODO: handle this case
+        result.invalid = true;
         return;
     }
 
@@ -150,13 +205,13 @@ static void makeElbow(Mesh &mesh, const Ball &ball, ResultQuad &result)
     for (int j = 0; j < 4; j++) {
         v[j] = ball.center + rotate(last.v[j] - child.center, rotationAxis, rotationAngle / 2) * scale;
     }
-    int i = mesh.vertices.count();
-    addSweep(mesh, last.i0, last.i1, last.i2, last.i3, v[0], v[1], v[2], v[3]);
+    addSegmentedSweep(mesh, last, v[0], v[1], v[2], v[3], child.maxRadius(), ball.maxRadius());
 
     // rotate 100% for the next step
     for (int j = 0; j < 4; j++) {
         v[j] = ball.center + rotate(last.v[j] - child.center, rotationAxis, rotationAngle) * scale;
     }
+    int i = mesh.vertices.count() - 4;
     result.setIndices(i, i + 1, i + 2, i + 3);
     result.setVertices(v[0], v[1], v[2], v[3]);
 }
@@ -178,7 +233,7 @@ static void makeJoint(Mesh &mesh, const Ball &ball, ResultQuad &result)
         for (int j = 0; j < 4; j++) {
             v[j] = ball.center + (child.center - ball.center).unit() * ball.maxRadius() + (last.v[j] - child.center) * scale;
         }
-        addSweep(mesh, last.i0, last.i1, last.i2, last.i3, v[0], v[1], v[2], v[3]);
+        addSegmentedSweep(mesh, last, v[0], v[1], v[2], v[3], child.maxRadius(), ball.maxRadius());
     }
 
     // TODO: remove this when stitching is implemented
